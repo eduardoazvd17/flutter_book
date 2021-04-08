66edc2c0255f0643890f7fd601ac2ce7761f58f1
Eduardo Azevedo <eduardoazvd17@gmail.com>
2021-04-08 19:01:42 -0300

    Criação do widget da postagem

--- lib/widgets/home_content.dart
+++ lib/widgets/home_content.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterbook/controllers/posts_controller.dart';
 import 'package:flutterbook/models/post_model.dart';
+import 'package:flutterbook/widgets/post_tile.dart';
 import 'package:get/get.dart';
 
 class HomeContent extends StatelessWidget {
@@ -14,9 +15,7 @@ class HomeContent extends StatelessWidget {
         itemCount: _postsController.posts.length,
         itemBuilder: (context, index) {
           PostModel post = _postsController.posts[index];
-          return ListTile(
-            title: Text(post.id),
-          );
+          return PostTile(post);
         },
       ),
     );
---
+++ lib/widgets/post_tile.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterbook/models/post_model.dart';
+
+class PostTile extends StatelessWidget {
+  final PostModel post;
+  PostTile(this.post);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(15),
+          topRight: Radius.circular(5),
+          bottomLeft: Radius.circular(5),
+          bottomRight: Radius.circular(15),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _author(),
+            SizedBox(height: 10),
+            Divider(),
+            SizedBox(height: 10),
+            _postContent(),
+            SizedBox(height: 20),
+            _postReview(),
+            SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _author() => Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipOval(
+            child: Image.network(
+              post.user.imageUrl,
+              width: 30,
+            ),
+          ),
+          SizedBox(width: 10),
+          Text(post.user.name),
+        ],
+      );
+
+  _postContent() => Column(
+        children: [
+          Text(
+            this.post.text.length > 288
+                ? this.post.text.substring(0, 284) + "..."
+                : this.post.text,
+            textAlign: TextAlign.start,
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                post.formattedDate,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
+
+  _postReview() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                post.isRead ? CupertinoIcons.eye : CupertinoIcons.eye_slash,
+                size: 20,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                CupertinoIcons.reply,
+                size: 20,
+              ),
+              Text(
+                post.answers.toString(),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                CupertinoIcons.square_favorites,
+                size: 20,
+              ),
+              Text(
+                post.version.toString(),
+              ),
+            ],
+          ),
+        ],
+      );
+}
