d7444fed08a89d0172863ea5262792fe14cd296a
Eduardo Azevedo <eduardoazvd17@gmail.com>
2021-04-08 16:02:12 -0300

    Fazendo busca da api e conversão para modelo

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,1 +1,24 @@
-class ApiService {}
+import 'package:dio/dio.dart';
+import 'package:flutterbook/models/post_model.dart';
+
+class ApiService {
+  final Dio _dio;
+  ApiService(this._dio);
+
+  Future<List<PostModel>> getPosts() async {
+    try {
+      List<PostModel> posts = [];
+      String url =
+          "https://run.mocky.io/v3/29ca1a88-ec02-44aa-9d26-214983b174a9";
+      Response response = await _dio.get(url);
+      Map result = response.data as Map;
+      for (Map post in result["Result"]) {
+        posts.add(PostModel.fromMap(post));
+      }
+      return posts;
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
+}
